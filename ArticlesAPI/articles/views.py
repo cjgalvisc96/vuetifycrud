0a568c9cb7b8eff3dd15fd5f3f07b873fe58d36f
cjgalvisc96 <cjgalvisc@unal.edu.co>
2021-08-04 21:22:27 -0500

    Articles API 1.0.1(simplify ArticleListAPIView)

--- ArticlesAPI/articles/views.py
+++ ArticlesAPI/articles/views.py
@@ -1,6 +1,4 @@
-from bson import ObjectId
-from rest_framework import generics, status
-from rest_framework.response import Response
+from rest_framework import generics
 from articles.models import Article
 from articles.serializers import ArticleSerializer
 
@@ -8,14 +6,6 @@ from articles.serializers import ArticleSerializer
 class ArticleListAPIView(generics.ListCreateAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
-
-    def post(self, request):
-        serializer = self.serializer_class(data = request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class ArticleRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
